263e309ce6f1a216747fa513ea026f2235187293
jean.laneurit <jean.laneurit@inrae.fr>
2022-03-10 16:48:37 +0100

    bringup : change path to romea_mobile_base_description

--- effibote3_bringup/launch/effibote3_gazebo.launch.py
+++ effibote3_bringup/launch/effibote3_gazebo.launch.py
@@ -66,14 +66,14 @@ def generate_launch_description():
 
     joint_state_broadcaster = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["joint_state_broadcaster"],
         output="screen",
     )
 
     diff_drive_controller = Node(
         package="controller_manager",
-        executable="spawner.py",
+        executable="spawner",
         arguments=["diff_drive_controller"],
         output="screen",
     )
@@ -84,7 +84,7 @@ def generate_launch_description():
             robot_state_publisher,
             spawn_entity,
             joint_state_broadcaster,
-            diff_drive_controller
+#            diff_drive_controller
 #            controller_manager,
         ]
     )
