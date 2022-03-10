79582688706586bc6b3134641f65825b5c5835b6
jean.laneurit <jean.laneurit@inrae.fr>
2022-03-10 16:47:35 +0100

    description : change romea_odo_description path to romea_mobile_base_description path

--- effibote3_description/launch/view_effibote3.launch.py
+++ effibote3_description/launch/view_effibote3.launch.py
@@ -5,12 +5,12 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 def generate_launch_description():
 
-    romea_odo_description_package_prefix = get_package_share_directory('romea_odo_description')
+    romea_mobile_base_description_package_prefix = get_package_share_directory('romea_mobile_base_description')
     urdf_file = get_package_share_directory("effibote3_description") + "/urdf/effibote3.urdf.xacro"
 
     return LaunchDescription([
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([romea_odo_description_package_prefix,'/launch/view_robot.launch.py']),
+            PythonLaunchDescriptionSource([romea_mobile_base_description_package_prefix,'/launch/view_robot.launch.py']),
             launch_arguments = {'urdf_file': urdf_file}.items(),
         ),
     ])
